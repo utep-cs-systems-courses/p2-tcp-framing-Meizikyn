3e0db1a929f49d68496de3b7e75b9599c7037f91
Vergil D. Meizikyn <meizikyn@gmail.com>
2021-03-07 10:15:20 -0700

    cleanup server

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 import socket, sys, re    
 from sefinalum import Sefinalum
+from logger import Logger
+
+log = Logger(4)
 
 def run():
     try:
@@ -11,7 +14,7 @@ def run():
         sock.listen(1)
         
         conn, addr = sock.accept()
-        print(f'[==] (ACCEPT) :: ADDR<{addr}>')
+        log.info('SOCK ACCEPT', f'ADDR: <{addr}>')
         
     
         units = []
@@ -25,7 +28,7 @@ def run():
             data = fsm['data']
             if 'msg' in fsm:
                 msg += fsm['msg']
-                print(f'[==] (RECEIVE) :: {msg}')
+                log.info('SOCK RECEIVE', 'msg')
 
             if result == 'end':
                 break
@@ -41,76 +44,6 @@ def run():
                 
     except KeyboardInterrupt:
         pass
-            
-    conn.shutdown(socket.SHUT_WR)
-    conn.close()
-
-def run_old():
-    try:
-        source_addr = ''
-        source_port = int(sys.argv[1])
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.bind((source_addr, source_port))
-        sock.listen(1)
-        
-        conn, addr = sock.accept()
-        print(f'[==] (ACCEPT) :: ADDR<{addr}>')
-        
-    
-        state = 'get_command'
-        units = []
-        tmp = ''
-    
-        while len(data := conn.recv(4).decode()):
-            tmp += data
-    
-            if state == 'get_command':
-                try:
-                    idx = tmp.index(';')
-                    tokens = tmp[:idx].split(' ')
-                    command = tokens[0]
-                    print(f'COMMAND: <{command}>')
-                    if command == 'end':
-                        print('end')
-                        break
-                    size = int(tokens[1])
-                    idz = size + idx + 1
-                    state = 'get_msg'
-                    continue
-                except ValueError:
-                    continue
-    
-            if state == 'get_size':
-                try: # ^size 11 Hello World
-                    size_idx = tmp[cmd_idx+1:].index(' ') + cmd_idx + 1
-                    size = int(tmp[cmd_idx+1:size_idx])
-                    end_idx = size + size_idx + 1
-                    print(f'SIZE: <{size}>')
-                    state = 'get_msg'
-                    continue
-                except ValueError:
-                    continue
-    
-            if state == 'get_msg':
-                if len(tmp[idx+1:]) >= size:
-                    msg = tmp[idx+1:idz]
-                    units += [msg]
-                    tmp = tmp[idz:]
-                    print(f'Remaining: <{tmp}>')
-                    state = 'get_command'
-                    print(f'MSG: <{msg}>')
-                    continue
-                continue
-           
-        
-        print('[==] (RECEIVE) :: {msg}')
-    
-        while len(msg):
-            sent = conn.send(msg.encode())
-            msg = msg[sent:]
-                
-    except KeyboardInterrupt:
-        conn.close()
             
     conn.shutdown(socket.SHUT_WR)
     conn.close()
