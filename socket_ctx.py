06d3d93c1b776c7008b73cf436d01c80a2859472
Vergil D. Meizikyn <meizikyn@gmail.com>
2021-03-07 19:57:02 -0700

    replace stub dbg with logger

--- socket_ctx.py
+++ socket_ctx.py
@@ -1,29 +1,31 @@
 #!/usr/bin/env python3
 
 import socket
+from logger import Logger
 
 class open_socket(object):
-    def __init__(self, server_host, server_port):
+    def __init__(self, server_host, server_port, log_level=2):
         self.sock = None
         self.server_host = server_host
         self.server_port = server_port
+        self.log = Logger(log_level)
         
     def __enter__(self):
         for addr_info in socket.getaddrinfo(self.server_host, self.server_port, socket.AF_UNSPEC, socket.SOCK_STREAM):
             addr_family, sock_type, proto, canonical_name, sock_addr = addr_info
             try:
                 self.sock = socket.socket(addr_family, sock_type, proto)
-                print(f'[==] (CREATE SOCKET) :: AF<{addr_family}> TYPE<{sock_type}> PROTOCOL<{proto}>')
+                self.log.debug('CREATE SOCKET' f'AF<{addr_family}> TYPE<{sock_type}> PROTOCOL<{proto}>')
             except socket.error as err:
-                print('[EE] (CREATE SOCKET) :: err')
+                self.log.error('SOCK CREATE FAILURE', err)
                 self.sock = None
                 continue
             
             try:
                 self.sock.connect(sock_addr)
-                print(f'[==] (CONNECT) :: TARGET<{sock_addr}>')
+                self.log.debug('SOCK CONNECT', f'TARGET<{sock_addr}>')
             except socket.error as err:
-                print(f'[EE] (CONNECT) :: {err}')
+                self.log.error('SOCK CONNECT FAILURE', err)
                 self.sock.close()
                 self.sock = None
                 continue
